bef3ff663c916a32dd9e2c40380388dbc4982fbe
graykode <nlkey2022@gmail.com>
2024-05-29 18:43:09 +0900

    fix: default gas limit

--- model/clober-market-maker.ts
+++ model/clober-market-maker.ts
@@ -822,7 +822,7 @@ export class CloberMarketMaker {
       value: [...bidMakeParams, ...askMakeParams]
         .filter((p) => p.isETH)
         .reduce((acc: bigint, { quoteAmount }) => acc + quoteAmount, 0n),
-      gas: 5_000_000n,
+      gas: 10_000_000n,
       gasPrice,
     })
     await waitTransaction(
