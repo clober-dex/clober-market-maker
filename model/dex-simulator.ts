87db14fb84de80e443fd0e7256a7c6ace4e3b869
Xavier <ladig1017@gmail.com>
2024-06-01 01:43:22 +0900

    feat: update when score is same with previous best spread

--- model/dex-simulator.ts
+++ model/dex-simulator.ts
@@ -226,7 +226,7 @@ export class DexSimulator {
         // calculate score
         const score = entropy.times(totalQuoteProfit)
 
-        if (score.comparedTo(bestSpreadPair.score) > 0) {
+        if (score.comparedTo(bestSpreadPair.score) >= 0) {
           bestSpreadPair.profit = totalQuoteProfit
           bestSpreadPair.askSideProfit = askSideQuoteProfit
           bestSpreadPair.bidSideProfit = bidSideQuoteProfit
