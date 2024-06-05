a98da8db6de94d67aaa4780e738c75e476448385
graykode <nlkey2022@gmail.com>
2024-06-05 22:50:22 +0900

    feat: add cache when covert timestamp to block number

--- utils/time.ts
+++ utils/time.ts
@@ -9,16 +9,29 @@ export const getDeadlineTimestampInSeconds = (): bigint => {
   return BigInt(Math.floor(currentTimestampInSeconds() + 60 * 60))
 }
 
+const cache: {
+  [chainId: string]: {
+    [timestamp: number]: bigint
+  }
+} = {}
+
 export const convertTimestampToBlockNumber = async (
   chainId: CHAIN_IDS,
   timestamp: number,
 ): Promise<bigint> => {
+  if (cache[chainId] && cache[chainId][timestamp]) {
+    return cache[chainId][timestamp]
+  }
   const response = await fetch(
     `https://coins.llama.fi/block/${CHAIN_MAP[chainId].name.toLowerCase()}/${timestamp}`,
   )
   const { height } = (await response.json()) as {
     height: number
     timestamp: number
+  }
+  cache[chainId] = {
+    ...cache[chainId],
+    [timestamp]: BigInt(height),
   }
   return BigInt(height)
 }
